8ef2ba21554ee5c6dcb57c80c1a04a83e500f310
SanchitJain28 <mythichuman28@gmail.com>
2025-08-20 18:49:28 +0530

    DSÀAaa

--- inserionSortRetry.js
+++ inserionSortRetry.js
@@ -10,16 +10,68 @@
 //   return array;
 // }
 
-function BubbleSort(array) {
+// function BubbleSort(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length - i; j++) {
+//       let temp = array[j]; //7
+//       if (array[j] > array[j + 1]) {
+//         array[j] = array[j + 1];
+//         array[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return array;
+// }
+// console.log(BubbleSort([7, 6, 5, 4, 3, 2, 1]));
+
+function mergeSort(array) {
+  let sortedArray = [];
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - i; j++) {
-      let temp = array[j]; //7
-      if (array[j] > array[j + 1]) {
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+    sortedArray.push([array[i]]);
+  }
+  while (sortedArray.length > 1) {
+    let newSet = [];
+    for (let i = 0; i < sortedArray.length; i += 2) {
+      let arr1 = sortedArray[i];
+      let arr2 = sortedArray[i + 1];
+      console.log(arr1, arr2);
+      if (arr2) {
+        let result = merge(arr1, arr2);
+        newSet.push(result);
+      } else {
+        newSet.push(arr1)
       }
     }
+    sortedArray = newSet;
   }
-  return array;
+
+  //lets make the merging fucntion
+  function merge(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+    let result = [];
+    for (; i < arr1.length && j < arr2.length; ) {
+      if (arr1[i] > arr2[j]) {
+        result.push(arr2[j]);
+        j++;
+      } else {
+        result.push(arr1[i]);
+        i++;
+      }
+    }
+
+    //PUSH THE RAMING ELEMENTS
+    for (; i < arr1.length; i++) {
+      result.push(arr1[i]);
+    }
+
+    for (; j < arr2.length; j++) {
+      result.push(arr2[j]);
+    }
+
+    return result
+  }
+
+  return sortedArray.flat();
 }
-console.log(BubbleSort([7, 6, 5, 4, 3, 2, 1]));
+console.log(mergeSort([7, 6, 5, 4, 3, 2, 1]));
